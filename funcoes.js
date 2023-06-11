eed934d0747ec9bf45b1d58916283a9f64159ffb
Gabriel Baptistella Maia <gabrielbmaiaa@gmail.com>
2023-06-11 14:42:27 -0300

    Alterações no JS

--- funcoes.js
+++ funcoes.js
@@ -69,13 +69,14 @@ function changePage(pageNum) {
         "</div>"+
         "<div class='row flex-lg-row align-items-center g-5 py-5'>"+
         "   <div class='col-10 col-sm-8 col-lg-6'>"+
-        " <h1 class='text-center'> MVP </h1>"+
+        " <h1 class='text-center' style='text-decoration: underline'> MVP </h1><br><br>"+
         "     <img src='img/campeonatos/cold.avif' width='400' height='300' style='border-radius: 15px; margin-left:50px'>"+
         "   </div>"+
         " <div class='col-lg-6'>"+
         "     <h3 class='display-7 fw-bold text-body-emphasis lh-1 mb-3'>Marcelo 'coldzera' David (Brasileiro)</h1>"+
         " </div>"+
         "</div>"+
+        "<h3 class='text-center'> Group Play</h3>"+
         "<div class='container text-center'>"+
           "<div class='row'>"+
             "<div class='col'>"+
@@ -297,8 +298,243 @@ function changePage(pageNum) {
       "</div>";
         break;
       case 1:
-        cardTitle.textContent = "Results";
-        cardText.innerHTML = "TESTE" ;
+        cardTitle.innerHTML = "<h2 class='text-center'> Resultados </h2>";
+        cardText.innerHTML = "<div class='container text-center'>"+
+        "<div class='row'>"+
+        "<div class=\"col-md-6\">"+
+        "<table class=\"table\">"+
+          "<h3 style=\"text-align: center;\"> Jogos (05/07/2016)</h3>"+
+          "<thead>"+
+            "<tr>"+
+              "<th scope=\"col\">Time</th>"+
+              "<th scope=\"col\">Placar</th>"+
+              "<th scope=\"col\">Time</th>"+
+            "</tr>"+
+          "</thead>"+
+          "<tbody>"+
+            "<tr>"+
+              "<td>Fnatic</td>"+
+              "<td> 14 x 16</td>"+
+              "<td>FaZe</td>"+
+            "</tr>"+
+            "<tr>"+
+              "<td>SK</td>"+
+              "<td> 16 x 11</td>"+
+              "<td>G2</td>"+
+            "</tr>"+
+            "<tr>"+
+              "<td>Virtus.pro</td>"+
+              "<td> 16 x 10</td>"+
+              "<td>MOUZ</td>"+
+            "</tr>"+
+            "<tr>"+
+              "<td>Liquid</td>"+
+              "<td> 16 x 7</td>"+
+              "<td>Envy</td>"+
+            "</tr>"+
+            "<tr>"+
+              "<td>Ninjas in Pyjamas</td>"+
+              "<td> 16 x 4</td>"+
+              "<td>OpTic</td>"+
+            "</tr>"+
+            "<tr>"+
+              "<td>Natus Vincere</td>"+
+              "<td> 16 x7</td>"+
+              "<td>FlipSid3</td>"+
+            "</tr>"+
+            "<tr>"+
+              "<td>CLG</td>"+
+              "<td> 13 x 16</td>"+
+              "<td>Gambit</td>"+
+            "</tr>"+
+            "<tr>"+
+              "<td>Astralis</td>"+
+              "<td> 16 x 12</td>"+
+              "<td>Dignitas</td>"+
+            "</tr>"+
+          "</tbody>"+
+        "</table>"+
+      "</div>"+
+      "<br><br>"+
+      "<div class=\"col-md-6\">"+
+        "<table class=\"table\">"+
+          "<h3 style=\"text-align: center;\"> Jogos (06/07/2016)</h3>"+
+          "<thead>"+
+            "<tr>"+
+              "<th scope=\"col\">Time</th>"+
+              "<th scope=\"col\">Placar</th>"+
+              "<th scope=\"col\">Time</th>"+
+            "</tr>"+
+          "</thead>"+
+          "<tbody>"+
+            "<tr>"+
+              "<td>G2</td>"+
+              "<td>13 x 16</td>"+
+              "<td>Fnatic</td>"+
+            "</tr>"+
+            "<tr>"+
+              "<td>SK</td>"+
+              "<td>16 x 6</td>"+
+              "<td>FaZe</td>"+
+            "</tr>"+
+            "<tr>"+
+              "<td>Envy</td>"+
+              "<td> 12 x 16</td>"+
+              "<td>MOUZ</td>"+
+            "</tr>"+
+            "<tr>"+
+              "<td>Liquid</td>"+
+              "<td>12 x 16</td>"+
+              "<td>Virtus.pro</td>"+
+            "</tr>"+
+            "<tr>"+
+              "<td>FlipSid3</td>"+
+              "<td>16 x 13</td>"+
+              "<td>OpTic</td>"+
+            "</tr>"+
+            "<tr>"+
+              "<td>Natus Vincere</td>"+
+              "<td> 16 x 12</td>"+
+              "<td>Ninjas in Pyjamas</td>"+
+            "</tr>"+
+            "<tr>"+
+              "<td>CLG</td>"+
+              "<td> 1 x 16</td>"+
+              "<td>Dignitas</td>"+
+            "</tr>"+
+            "<tr>"+
+              "<td>Astralis</td>"+
+              "<td> 6 x 16 </td>"+
+              "<td>Gambit</td>"+
+            "</tr>"+
+          "</tbody>"+
+        "</table>"+
+      "</div>"+
+      "</div>"+
+      "<br><br>"+
+      "<div class='container text-center'>"+
+      "<div class='row'>"+
+      "<div class=\"col-md-6\">"+
+        "<table class=\"table\">"+
+          "<h3 style=\"text-align: center;\"> Jogos (07/07/2016) - Melhor de 3</h3>"+
+          "<thead>"+
+            "<tr>"+
+              "<th scope=\"col\">Time</th>"+
+              "<th scope=\"col\">Placar</th>"+
+              "<th scope=\"col\">Time</th>"+
+            "</tr>"+
+          "</thead>"+
+          "<tbody>"+
+            "<tr>"+
+              "<td>FaZe</td>"+
+              "<td>0 x 2</td>"+
+              "<td>Fnatic</td>"+
+            "</tr>"+
+            "<tr>"+
+              "<td>Liquid</td>"+
+              "<td>2 x 0</td>"+
+              "<td>MOUZ</td>"+
+            "</tr>"+
+            "<tr>"+
+              "<td>Ninjas in Pyjamas</td>"+
+              "<td>1 x 2</td>"+
+              "<td>FlipSid3</td>"+
+            "</tr>"+
+            "<tr>"+
+              "<td>Astralis</td>"+
+              "<td>2 x 1</td>"+
+              "<td>Dignitas</td>"+
+            "</tr>"+
+          "</tbody>"+
+        "</table>"+
+      "</div>"+
+      "<br><br>"+
+      "<div class=\"col-md-6\">"+
+        "<table class=\"table\">"+
+          "<h3 style=\"text-align: center;\"> Jogos (08/07/2016) - Melhor de 3</h3>"+
+          "<thead>"+
+            "<tr>"+
+              "<th scope=\"col\">Time</th>"+
+              "<th scope=\"col\">Placar</th>"+
+              "<th scope=\"col\">Time</th>"+
+            "</tr>"+
+          "</thead>"+
+          "<tbody>"+
+            "<tr>"+
+              "<td>Gambit</td>"+
+              "<td>0 x 2</td>"+
+              "<td>Fnatic</td>"+
+            "</tr>"+
+            "<tr>"+
+              "<td>Natus Vincere</td>"+
+              "<td>1 x 2</td>"+
+              "<td>Liquid</td>"+
+            "</tr>"+
+            "<tr>"+
+              "<td>SK</td>"+
+              "<td>2 x 0</td>"+
+              "<td>FlipSid3</td>"+
+            "</tr>"+
+            "<tr>"+
+              "<td>Virtus.pro</td>"+
+              "<td>2 x 0</td>"+
+              "<td>Astralis</td>"+
+            "</tr>"+
+          "</tbody>"+
+        "</table>"+
+      "</div>"+
+      "</div>"+
+      "<br><br>"+
+      "<div class='container text-center'>"+
+      "<div class='row justify-content-md-center'>"+
+      "<div class=\"col-md-6\">"+
+        "<table class=\"table\">"+
+          "<h3 style=\"text-align: center;\"> Jogos (09/07/2016) - Melhor de 3</h3>"+
+          "<thead>"+
+            "<tr>"+
+              "<th scope=\"col\">Time</th>"+
+              "<th scope=\"col\">Placar</th>"+
+              "<th scope=\"col\">Time</th>"+
+            "</tr>"+
+          "</thead>"+
+          "<tbody>"+
+            "<tr>"+
+              "<td>Liquid</td>"+
+              "<td>2 x 0</td>"+
+              "<td>Fnatic</td>"+
+            "</tr>"+
+            "<tr>"+
+              "<td>Virtus.pro</td>"+
+              "<td>1 x 2</td>"+
+              "<td>SK</td>"+
+            "</tr>"+
+          "</tbody>"+
+        "</table>"+
+      "</div>"+
+      "</div>"+
+      "<br><br>"+
+      "<div class='container text-center'>"+
+      "<div class='row justify-content-md-center'>"+
+      "<div class=\"col-md-6\">"+
+        "<table class=\"table\">"+
+          "<h3 style=\"text-align: center;\"> Jogos (10/07/2016) - Final</h3>"+
+          "<thead>"+
+            "<tr>"+
+              "<th scope=\"col\">Time</th>"+
+              "<th scope=\"col\">Placar</th>"+
+              "<th scope=\"col\">Time</th>"+
+            "</tr>"+
+          "</thead>"+
+          "<tbody>"+
+            "<tr>"+
+              "<td>SK</td>"+
+              "<td>2 x 0</td>"+
+              "<td>Liquid</td>"+
+            "</tr>"+
+          "</tbody>"+
+        "</table>"+
+      "</div>"+
+      "</div>";
         break;
       case 2:
         cardTitle.textContent = "Stats";
